2bad262502e8af95863a16df37ee12ee3e2ac660
zkl <1090890128@qq.com>
2020-04-03 14:32:17 +0800

    222

---
+++ JwtDemo/Controllers/JwtController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtDemo.Controllers
+{
+    
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/[controller]/[action]")]
+        public IActionResult Get(string userName, string pwd)
+        {
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(pwd))
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}") ,
+                    new Claim (JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddMinutes(30)).ToUnixTimeSeconds()}"),
+                    new Claim(ClaimTypes.Name, userName)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Const.SecurityKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    issuer: Const.Domain,
+                    audience: Const.Domain,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            else
+            {
+                return BadRequest(new { message = "username or password is incorrect." });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/[action]")]
+        [Authorize]
+        public IActionResult UserCenter()
+        {
+            return Ok("1111");
+        }
+    }
+
+    public class Const
+    {
+        /// <summary>
+        /// 这里为了演示，写死一个密钥。实际生产环境可以从配置文件读取,这个是用网上工具随便生成的一个密钥
+        /// </summary>
+        public const string SecurityKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDI2a2EJ7m872v0afyoSDJT2o1+SitIeJSWtLJU8/Wz2m7gStexajkeD+Lka6DSTy8gt9UwfgVQo6uKjVLG5Ex7PiGOODVqAEghBuS7JzIYU5RvI543nNDAPfnJsas96mSA7L/mD7RTE2drj6hf3oZjJpMPZUQI/B1Qjb5H3K3PNwIDAQAB";
+        public const string Domain = "http://localhost:5000";
+    }
+}
